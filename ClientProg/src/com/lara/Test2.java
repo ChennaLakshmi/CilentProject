073576d7f80154d30462ed8271a55cbb1a8c7b7f
ChennaLakshmi <chennalakshmi0408@gmail.com>
2019-12-27 14:50:12 +0530

    first commit
    upload all files

---
+++ ClientProg/src/com/lara/Test2.java
@@ -0,0 +1,15 @@
+package com.lara;
+
+public class Test2 {
+	static int a;
+	static int b;
+	static int sum;
+public static void main(String[] args)
+{
+	a = 10;
+	b = 20;
+	sum = a + b;
+	System.out.println(sum);
+	
+}
+}
